d32b2879e1d8bbf0ac78b140e68e6106da0bdba3
Jacek Pasierb <jack_byk@o2.pl>
2023-02-16 22:50:43 +0100

    Created two script

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (const ingredient of ingredients) {
+  
+  const list = document.createElement("li");
+  list.textContent = ingredient;
+  list.classList.add("item");
+
+  
+  const arr = document.querySelector("#ingredients");
+  arr.append(list);
+
+ 
+}
